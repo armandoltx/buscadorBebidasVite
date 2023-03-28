2f68671ad928c60c3bb02822198315162a79604c
Armando Ruiz <armando.ruiz@industrie.co>
2023-03-28 16:02:40 +1100

    360. Finalizando el proyecto; quitando el modal antes de ensenar otro

--- src/context/BebidasProvider.jsx
+++ src/context/BebidasProvider.jsx
@@ -8,8 +8,10 @@ const BebidasProvider = ({children}) => {
   const [modal, setModal] = useState(false)
   const [bebidaId, setBebidaId] = useState(null)
   const [receta, setReceta] = useState({})
+  const [cargando, setCargando] = useState(false)
 
   useEffect(() => {
+    setCargando(true)
     const obtenerReceta = async () => {
       if(!bebidaId) return
       try {
@@ -19,6 +21,8 @@ const BebidasProvider = ({children}) => {
         setReceta(data.drinks[0])
       } catch (error) {
         console.log(error)
+      } finally {
+        setCargando(false)
       }
     }
 
@@ -53,7 +57,8 @@ const BebidasProvider = ({children}) => {
         modal,
         handleModalClick,
         handleBebidaIdclick,
-        receta
+        receta,
+        cargando
       }}
     >
       {children}
