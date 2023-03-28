569be7f2dd797ad44f13d2f7a51d6c0c22d386f7
Armando Ruiz <armando.ruiz@industrie.co>
2023-03-28 11:21:55 +1100

    350. Obteniendo las categorias de la API

--- src/context/CategoriasProvider.jsx
+++ src/context/CategoriasProvider.jsx
@@ -1,12 +1,31 @@
 import React, { useState, useEffect, createContext } from 'react'
+import axios from 'axios'
 
 const categoriasContext = createContext()
 
 const CategoriasProvider = ({children}) => {
+  const [categorias, setCategorias] = useState([])
+
+  const obtenerCategorias = async () => {
+    try {
+      const url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list"
+      const { data } = await axios(url)
+      // console.log(data)
+      setCategorias(data.drinks)
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    obtenerCategorias()
+  },[])
 
   return(
     <categoriasContext.Provider
-      value={{}}
+      value={{
+        categorias
+      }}
     >
       {children}
     </categoriasContext.Provider>
